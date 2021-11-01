af8280bc353aa558e468a07d3c45de78a400bed5
Clairo Dorneles <clairodorneles@Clairos-MacBook-Pro.local>
2021-11-01 15:55:54 -0300

    add files to AWS SUBNET MODULE

---
+++ readme.md
@@ -0,0 +1,15 @@
+# AWS Terraform Module
+
+## SUBNET
+
+## Inputs:
+
+VPC Id - vpc_Id
+Subnet Name - subnet_name
+Subnet Cidr Block - subnet_cidr
+Subnet Type -  subnet_type
+
+
+## Outputs
+
+Subnet Id
---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "vpc_id" {
+    description = "VPC ID"
+}
+variable "subnet_cidr" {
+    description = "cidr block"
+}
+variable "subnet_type" {
+    description = "Subnet public or private"
+}
+variable "subnet_name" {
+    description = "Subnet name"
+}
